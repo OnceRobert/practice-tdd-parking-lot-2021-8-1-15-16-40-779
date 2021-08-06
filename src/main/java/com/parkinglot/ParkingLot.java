86e91304eb6ebf92dced9bcb4fd9c2733b724d9d
OnceRobert <robpurugganan@gmail.com>
2021-08-06 14:47:39 +0800

    [Story 1][Robert] feat: implemented should_return_correct_cars_when_fetch_twice_given_a_parking_lot_with_two_parked_car_and_two_parking_tickets

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,19 +1,25 @@
 package com.parkinglot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
-    private Car car;
+    //private Car car;
+    private Map<ParkingTicket,Car> unclaimedCars = new HashMap<>();
     public ParkingTicket park(Car car) {
-        this.car = car;
+        //this.car = car;
+        ParkingTicket newTicket = new ParkingTicket();
+        unclaimedCars.put(newTicket, car);
         //return null;
-        return new ParkingTicket();
+        return newTicket;
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
         if(parkingTicket == null)
             return null;
         else {
-            Car fetchedCar = this.car;
-            car = null;
+            Car fetchedCar = unclaimedCars.get(parkingTicket);
+            unclaimedCars.remove(parkingTicket);
             return fetchedCar;
         }
     }
