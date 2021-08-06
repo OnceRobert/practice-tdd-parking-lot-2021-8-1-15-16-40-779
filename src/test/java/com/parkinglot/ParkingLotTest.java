fd45b305f07bfc408c42da17646f864ccf3cf270
OnceRobert <robpurugganan@gmail.com>
2021-08-06 11:53:01 +0800

    [Story 1][Robert] test: should_return_car_when_fetch_given_a_parking_lot_with_a_parked_car_and_a_parking_ticket

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,11 +2,13 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class ParkingLotTest {
 
 
     @Test
-    void should_return parking_ticket_when_park_given_a_parking_lot_and_a_car()
+    public void should_return_parking_ticket_when_park_given_a_parking_lot_and_a_car()
     {
         //given
         ParkingLot parkingLot = new ParkingLot();
@@ -16,6 +18,22 @@ public class ParkingLotTest {
 
         //then
         assertNotNull(parkingTicket);
+
+    }
+
+    @Test
+    public void should_return_car_when_fetch_given_a_parking_lot_with_a_parked_car_and_a_parking_ticket()
+    {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        ParkingTicket parkingTicket = parkingLot.park(car);
+
+        //when
+        Car actualCar = parkingLot.fetch(car);
+
+        //then
+        assertEquals(car, actualCar);
 
     }
 }
