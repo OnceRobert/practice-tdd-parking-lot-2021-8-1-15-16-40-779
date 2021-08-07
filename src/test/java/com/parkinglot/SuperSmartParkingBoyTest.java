e7d17aab0325e2c8c61aac0e4c3ed8b18a0cd8e0
OnceRobert <robpurugganan@gmail.com>
2021-08-07 19:30:39 +0800

    [Story 6][Robert] test/feat: added 4 more of previous tests from smart parking boy to super smart parking boy test, these were also covered in the previous implementations

--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -41,76 +41,76 @@ public class SuperSmartParkingBoyTest {
         assertNotNull(chaeryeongTicket);
     }
 
-//    @Test
-//    void should_correct_cars_when_super_smart_parking_boy_fetch_twice_given_two_parking_lots_with_car_parked_two_parking_tickets()
-//    {
-//        //given
-//        ParkingLot tdoongParkingLot = new ParkingLot(1);
-//        ParkingLot itzyParkingLot = new ParkingLot(5);
-//        Car jihyoCar = new Car();
-//        Car chaeryeongCar = new Car();
-//        SuperSmartParkingBoy danielParkingBoy = new SuperSmartParkingBoy();
-//        danielParkingBoy.addParkingLot(tdoongParkingLot);
-//        danielParkingBoy.addParkingLot(itzyParkingLot);
-//        ParkingTicket jihyoTicket = danielParkingBoy.park(jihyoCar);
-//        ParkingTicket chaeryeongTicket = danielParkingBoy.park(chaeryeongCar);
-//        //when & then
-//        assertEquals(jihyoCar, danielParkingBoy.fetch(jihyoTicket));
-//        assertEquals(chaeryeongCar, danielParkingBoy.fetch(chaeryeongTicket));
-//    }
-//
-//    @Test
-//    void should_no_car_when_super_smart_parking_boy_fetch_given_two_parking_lots_with_invalid_parking_ticket()
-//    {
-//        //given
-//        ParkingLot tdoongParkingLot = new ParkingLot(1);
-//        ParkingLot itzyParkingLot = new ParkingLot(5);
-//        SuperSmartParkingBoy danielParkingBoy = new SuperSmartParkingBoy();
-//        danielParkingBoy.addParkingLot(tdoongParkingLot);
-//        danielParkingBoy.addParkingLot(itzyParkingLot);
-//        ParkingTicket unrecognizedTicket = new ParkingTicket();
-//
-//        //when & then
-//        assertThrows(unrecognizedParkingTicketException.class, () -> danielParkingBoy.fetch(unrecognizedTicket));
-//    }
-//
-//    @Test
-//    void should_return_unrecognized_ticket_exception_when_super_smart_parking_boy_fetch_given_two_parking_lots_and_used_parking_ticket()
-//    {
-//        //given
-//        ParkingLot tdoongParkingLot = new ParkingLot(1);
-//        ParkingLot itzyParkingLot = new ParkingLot(5);
-//        SuperSmartParkingBoy danielParkingBoy = new SuperSmartParkingBoy();
-//        danielParkingBoy.addParkingLot(tdoongParkingLot);
-//        danielParkingBoy.addParkingLot(itzyParkingLot);
-//
-//        Car jihyoCar = new Car();
-//        ParkingTicket jihyoTicket = danielParkingBoy.park(jihyoCar);
-//        jihyoCar = danielParkingBoy.fetch(jihyoTicket);
-//
-//        //when & then
-//        assertThrows(unrecognizedParkingTicketException.class, () -> danielParkingBoy.fetch(jihyoTicket));
-//    }
-//
-//    @Test
-//    void should_return_no_available_positions_when_super_smart_parking_boy_park_given_two_parking_lots_at_full_capacity_and_car()
-//    {
-//        //given
-//        ParkingLot tdoongParkingLot = new ParkingLot(1);
-//        ParkingLot itzyParkingLot = new ParkingLot(1);
-//        Car jihyoCar = new Car();
-//        Car chaeryeongCar = new Car();
-//        Car excessCar = new Car();
-//        SuperSmartParkingBoy danielParkingBoy = new SuperSmartParkingBoy();
-//        danielParkingBoy.addParkingLot(tdoongParkingLot);
-//        danielParkingBoy.addParkingLot(itzyParkingLot);
-//        ParkingTicket jihyoTicket = danielParkingBoy.park(jihyoCar);
-//        ParkingTicket chaeryeongTicket = danielParkingBoy.park(chaeryeongCar);
-//        //when & then
-//
-//        assertThrows(fullParkingLotException.class, () -> danielParkingBoy.park(excessCar));
-//    }
-//
+    @Test
+    void should_correct_cars_when_super_smart_parking_boy_fetch_twice_given_two_parking_lots_with_car_parked_two_parking_tickets()
+    {
+        //given
+        ParkingLot tdoongParkingLot = new ParkingLot(1);
+        ParkingLot itzyParkingLot = new ParkingLot(5);
+        Car jihyoCar = new Car();
+        Car chaeryeongCar = new Car();
+        SuperSmartParkingBoy danielParkingBoy = new SuperSmartParkingBoy();
+        danielParkingBoy.addParkingLot(tdoongParkingLot);
+        danielParkingBoy.addParkingLot(itzyParkingLot);
+        ParkingTicket jihyoTicket = danielParkingBoy.park(jihyoCar);
+        ParkingTicket chaeryeongTicket = danielParkingBoy.park(chaeryeongCar);
+        //when & then
+        assertEquals(jihyoCar, danielParkingBoy.fetch(jihyoTicket));
+        assertEquals(chaeryeongCar, danielParkingBoy.fetch(chaeryeongTicket));
+    }
+
+    @Test
+    void should_no_car_when_super_smart_parking_boy_fetch_given_two_parking_lots_with_invalid_parking_ticket()
+    {
+        //given
+        ParkingLot tdoongParkingLot = new ParkingLot(1);
+        ParkingLot itzyParkingLot = new ParkingLot(5);
+        SuperSmartParkingBoy danielParkingBoy = new SuperSmartParkingBoy();
+        danielParkingBoy.addParkingLot(tdoongParkingLot);
+        danielParkingBoy.addParkingLot(itzyParkingLot);
+        ParkingTicket unrecognizedTicket = new ParkingTicket();
+
+        //when & then
+        assertThrows(unrecognizedParkingTicketException.class, () -> danielParkingBoy.fetch(unrecognizedTicket));
+    }
+
+    @Test
+    void should_return_unrecognized_ticket_exception_when_super_smart_parking_boy_fetch_given_two_parking_lots_and_used_parking_ticket()
+    {
+        //given
+        ParkingLot tdoongParkingLot = new ParkingLot(1);
+        ParkingLot itzyParkingLot = new ParkingLot(5);
+        SuperSmartParkingBoy danielParkingBoy = new SuperSmartParkingBoy();
+        danielParkingBoy.addParkingLot(tdoongParkingLot);
+        danielParkingBoy.addParkingLot(itzyParkingLot);
+
+        Car jihyoCar = new Car();
+        ParkingTicket jihyoTicket = danielParkingBoy.park(jihyoCar);
+        jihyoCar = danielParkingBoy.fetch(jihyoTicket);
+
+        //when & then
+        assertThrows(unrecognizedParkingTicketException.class, () -> danielParkingBoy.fetch(jihyoTicket));
+    }
+
+    @Test
+    void should_return_no_available_positions_when_super_smart_parking_boy_park_given_two_parking_lots_at_full_capacity_and_car()
+    {
+        //given
+        ParkingLot tdoongParkingLot = new ParkingLot(1);
+        ParkingLot itzyParkingLot = new ParkingLot(1);
+        Car jihyoCar = new Car();
+        Car chaeryeongCar = new Car();
+        Car excessCar = new Car();
+        SuperSmartParkingBoy danielParkingBoy = new SuperSmartParkingBoy();
+        danielParkingBoy.addParkingLot(tdoongParkingLot);
+        danielParkingBoy.addParkingLot(itzyParkingLot);
+        ParkingTicket jihyoTicket = danielParkingBoy.park(jihyoCar);
+        ParkingTicket chaeryeongTicket = danielParkingBoy.park(chaeryeongCar);
+        //when & then
+
+        assertThrows(fullParkingLotException.class, () -> danielParkingBoy.park(excessCar));
+    }
+
 //    @Test
 //    void should_park_in_parking_lot_two_when_super_smart_parking_boy_park_given_parking_lot_with_two_cars_and_four_max_capacity_and_a_parking_lot_with_one_car_and_four_max_capacity()
 //    {
