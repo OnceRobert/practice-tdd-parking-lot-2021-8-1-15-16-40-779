e611f1cde9d81edb3a2e293283300daa2afbf1c0
OnceRobert <robpurugganan@gmail.com>
2021-08-07 19:20:20 +0800

    [Story 6][Robert] test/feat: should_parked_cars_one_is_greater_than_parked_cars_two_when_super_smart_parking_boy_park_given_two_parking_lots_and_car, and should_return_parking_ticket_when_super_smart_parking_boy_park_given_a_parking_lot_at_full_capacity_and_a_parking_lot_with_available_space, these were also covered in the previous implementation

--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -24,25 +24,25 @@ public class SuperSmartParkingBoyTest {
         assertEquals(1,danielParkingBoy.getParkingLots().get(0).getNumberParkedCars());
     }
 
+    @Test
+    void should_return_parking_ticket_when_super_smart_parking_boy_park_given_a_parking_lot_at_full_capacity_and_a_parking_lot_with_available_space()
+    {
+        //given
+        ParkingLot tdoongParkingLot = new ParkingLot(1);
+        ParkingLot itzyParkingLot = new ParkingLot(5);
+        Car jihyoCar = new Car();
+        Car chaeryeongCar = new Car();
+        SuperSmartParkingBoy danielParkingBoy = new SuperSmartParkingBoy();
+        danielParkingBoy.addParkingLot(tdoongParkingLot);
+        danielParkingBoy.addParkingLot(itzyParkingLot);
+        ParkingTicket jihyoTicket = danielParkingBoy.park(jihyoCar);
+        ParkingTicket chaeryeongTicket = danielParkingBoy.park(chaeryeongCar);
+        //when & then
+        assertNotNull(chaeryeongTicket);
+    }
+
 //    @Test
-//    void should_return_parking_ticket_when_smart_parking_boy_park_given_a_parking_lot_at_full_capacity_and_a_parking_lot_with_available_space()
-//    {
-//        //given
-//        ParkingLot tdoongParkingLot = new ParkingLot(1);
-//        ParkingLot itzyParkingLot = new ParkingLot(5);
-//        Car jihyoCar = new Car();
-//        Car chaeryeongCar = new Car();
-//        SuperSmartParkingBoy danielParkingBoy = new SuperSmartParkingBoy();
-//        danielParkingBoy.addParkingLot(tdoongParkingLot);
-//        danielParkingBoy.addParkingLot(itzyParkingLot);
-//        ParkingTicket jihyoTicket = danielParkingBoy.park(jihyoCar);
-//        ParkingTicket chaeryeongTicket = danielParkingBoy.park(chaeryeongCar);
-//        //when & then
-//        assertNotNull(chaeryeongTicket);
-//    }
-//
-//    @Test
-//    void should_correct_cars_when_smart_parking_boy_fetch_twice_given_two_parking_lots_with_car_parked_two_parking_tickets()
+//    void should_correct_cars_when_super_smart_parking_boy_fetch_twice_given_two_parking_lots_with_car_parked_two_parking_tickets()
 //    {
 //        //given
 //        ParkingLot tdoongParkingLot = new ParkingLot(1);
@@ -60,7 +60,7 @@ public class SuperSmartParkingBoyTest {
 //    }
 //
 //    @Test
-//    void should_no_car_when_smart_parking_boy_fetch_given_two_parking_lots_with_invalid_parking_ticket()
+//    void should_no_car_when_super_smart_parking_boy_fetch_given_two_parking_lots_with_invalid_parking_ticket()
 //    {
 //        //given
 //        ParkingLot tdoongParkingLot = new ParkingLot(1);
@@ -75,7 +75,7 @@ public class SuperSmartParkingBoyTest {
 //    }
 //
 //    @Test
-//    void should_return_unrecognized_ticket_exception_when_smart_parking_boy_fetch_given_two_parking_lots_and_used_parking_ticket()
+//    void should_return_unrecognized_ticket_exception_when_super_smart_parking_boy_fetch_given_two_parking_lots_and_used_parking_ticket()
 //    {
 //        //given
 //        ParkingLot tdoongParkingLot = new ParkingLot(1);
@@ -93,7 +93,7 @@ public class SuperSmartParkingBoyTest {
 //    }
 //
 //    @Test
-//    void should_return_no_available_positions_when_smart_parking_boy_park_given_two_parking_lots_at_full_capacity_and_car()
+//    void should_return_no_available_positions_when_super_smart_parking_boy_park_given_two_parking_lots_at_full_capacity_and_car()
 //    {
 //        //given
 //        ParkingLot tdoongParkingLot = new ParkingLot(1);
@@ -112,7 +112,7 @@ public class SuperSmartParkingBoyTest {
 //    }
 //
 //    @Test
-//    void should_park_in_parking_lot_two_when_smart_parking_boy_park_given_parking_lot_with_two_cars_and_four_max_capacity_and_a_parking_lot_with_one_car_and_four_max_capacity()
+//    void should_park_in_parking_lot_two_when_super_smart_parking_boy_park_given_parking_lot_with_two_cars_and_four_max_capacity_and_a_parking_lot_with_one_car_and_four_max_capacity()
 //    {
 //        //given
 //        ParkingLot tdoongParkingLot = new ParkingLot(4);
@@ -142,7 +142,7 @@ public class SuperSmartParkingBoyTest {
 //    }
 //
 //    @Test
-//    void should_park_in_parking_lot_one_when_smart_parking_boy_park_given_parking_lot_with_one_car_and_four_max_capacity_and_a_parking_lot_with_three_cars_and_four_max_capacity()
+//    void should_park_in_parking_lot_one_when_super_smart_parking_boy_park_given_parking_lot_with_one_car_and_four_max_capacity_and_a_parking_lot_with_three_cars_and_four_max_capacity()
 //    {//Given a smart parking boy, a parking lot with 1 parked cars and 4 max capacity and a parking lot with 3 parked car and 4 max capacity
 //        //given
 //        ParkingLot tdoongParkingLot = new ParkingLot(4);
